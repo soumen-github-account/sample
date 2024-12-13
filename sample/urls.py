4670582d487574eb5612efb970e084ee5765674f
soumen-github-account <soumendas7584@gmail.com>
2024-12-13 21:35:06 +0530

    Update urls.py

--- sample/urls.py
+++ sample/urls.py
@@ -27,5 +27,7 @@ urlpatterns = [
     path('message', mess , name="message"),
     path('admin/', admin.site.urls),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.STATIC_ROOT)
