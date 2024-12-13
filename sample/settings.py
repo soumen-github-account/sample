e08cb3237164a648acb963e8584c94adbca7093b
soumen-github-account <soumendas7584@gmail.com>
2024-12-13 21:34:14 +0530

    Update settings.py

--- sample/settings.py
+++ sample/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-3kxl6m^17gxb0@*f6&$^*kh7of=!0=(_8265+z*erznmqf00l@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = [".vercel.app", ".now.sh","127.0.0.1","localhost"]
 
 
 # Application definition
@@ -129,8 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = os.path.join[(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "build_files/staticfiles")
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
